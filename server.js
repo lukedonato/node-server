67de2194ebc833a421ab06e1500723e0c0b316ac
Luke <luke.donato@tivix.com>
2017-04-17 14:56:42 -0400

    add projects page

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-})
-
 app.use(express.static(`${__dirname}/public`));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -44,6 +40,10 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {pageTitle: 'About Page'});
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {pageTitle: 'Projects'});
 });
 
 app.get('/bad', (req, res) => {
